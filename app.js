c0f02626d25e5958e7e0bace282648f604699114
Juan <juancarlosmiranda122@gmail.com>
2026-07-30 19:57:42 -0500

    ejercicios2

--- app.js
+++ app.js
@@ -26,34 +26,56 @@ console.log(12/5)
 // console.log('tu salario en peso colombianos es de ' + salario / dolar )
 
 
-let peso = prompt('cual es tu peso?')
+// let peso = prompt('cual es tu peso?')
 
-console.log('Tu peso en venus es de ' + peso * 91 / 100);
-console.log('tu peso en marte es de ' + peso * 38 /100);
-console.log('tu peso en la luna es de ' + peso * 16.6 /100);
-
-
-
-
-let base = prompt('cual es la base del triangulo?');
-
-let altura = prompt('cual es la altura del triangulo?');
-
-let area = base * altura / 2;
-
-console.log('El area de tu riangulo es de: ' + area);
+// console.log('Tu peso en venus es de ' + peso * 91 / 100);
+// console.log('tu peso en marte es de ' + peso * 38 /100);
+// console.log('tu peso en la luna es de ' + peso * 16.6 /100);
 
 
 
-let gradosc = prompt('Cuales son los grados de esta sala en celcius? ');
 
-let gradosf = gradosc * 1.8 + 32;
+// let base = prompt('cual es la base del triangulo?');
 
-console.log('La temperatura de la habitacion en Fahrenheit  es de: ' + gradosf);
+// let altura = prompt('cual es la altura del triangulo?');
+
+// let area = base * altura / 2;
+
+// console.log('El area de tu riangulo es de: ' + area);
 
 
-let km = prompt('cuantos kilometros has recorrido hoy?' );
 
-let metros = km * 1000;
+// let gradosc = prompt('Cuales son los grados de esta sala en celcius? ');
 
-console.log('Recorriste ' + metros + 'metros' );
+// let gradosf = gradosc * 1.8 + 32;
+
+// console.log('La temperatura de la habitacion en Fahrenheit  es de: ' + gradosf);
+
+
+// let km = prompt('cuantos kilometros has recorrido hoy?' );
+
+// let metros = km * 1000;
+
+// console.log('Recorriste ' + metros + 'metros' );
+
+
+// let precio = Number(prompt('Cual es el precio de tu producto?'));
+
+// let descuento = precio * 20 / 100;
+
+// console.log('El precio de tu producto con el 20% de descuento es de: ' + descuento);
+
+
+let a = Number(prompt('cuanto te sacaste en la primer nota?'));
+let b = Number(prompt('cuanto te sacaste en la segunda nota?'));
+let c = Number(prompt('cuanto te sacaste en la tercera nota?'));
+
+let promedio = a + b + c / 3;
+
+console.log('Su promedio de las notas es de: ' + promedio);
+
+
+
+
+
+
